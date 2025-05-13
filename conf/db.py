6025dc8d3c2816921cc4e36f9be306a6ef8c3594
Dxin00 <dxin00@gmail.com>
2025-05-13 06:11:43 +0200

    WIP: save changes before rebase

--- conf/db.py
+++ conf/db.py
@@ -1,10 +1,13 @@
 """Database configuration and session management."""
 
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:567234@db:5432/hw02"
+load_dotenv()  # Загружаем переменные из .env
 
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, max_overflow=5)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
